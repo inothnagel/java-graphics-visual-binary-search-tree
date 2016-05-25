6c6020459726a1a58a28f776cdbb55ccb23dd912
Ignatius Nothnagel <inothnagel@gmail.com>
2016-05-25 19:33:38 +0200

    WIP: Start rendering tree

--- src/com/inothnagel/VisualBinarySearchTree/Main.java
+++ src/com/inothnagel/VisualBinarySearchTree/Main.java
@@ -6,7 +6,11 @@ import java.awt.*;
 import static java.lang.Thread.sleep;
 
 public class Main extends JFrame{
-    private Canvas canvas= new Canvas();
+    private Canvas canvas = new Canvas();
+    private Node rootNode;
+    private Point startingPoint;
+    private double rangeX = 10000;
+    private double rangeY = 10000;
 
     private Main(){
         super();
@@ -15,6 +19,12 @@ public class Main extends JFrame{
         add(canvas);
         setVisible(true);
         canvas.createBufferStrategy(2);
+
+        rootNode = initTree();
+    }
+
+    private Node initTree() {
+        return new Node(13);
     }
 
     public static void main(String arg[]) throws InterruptedException {
@@ -22,22 +32,62 @@ public class Main extends JFrame{
         main.loop();
     }
 
-    private void moveBlobs() {
-
-    }
-
     private void loop() throws InterruptedException {
         while (true) {
             Graphics graphics = canvas.getBufferStrategy().getDrawGraphics();
             clearBackground(graphics);
-            // move action here
+            renderTree(graphics, rootNode);
             canvas.getBufferStrategy().show();
             sleep(40);
         }
     }
 
+    private void renderTree(Graphics graphics, Node rootNode) {
+        recursiveDrawNode(graphics, rootNode, startingPoint);
+    }
+
+    private void recursiveDrawNode(Graphics graphics, Node node, Point point) {
+        drawNode(graphics, node, point);
+    }
+
+    private void drawNode(Graphics graphics, Node node, Point point) {
+        graphics.fillOval(displayX(point.getX()), displayY(point.getY()), pixelWidth(node.width()), pixelHeight(node.Height()));
+    }
+
     private void clearBackground(Graphics graphics) {
         graphics.setColor(new Color(0, 0, 0));
         graphics.fillRect(0,0,getSize().width, getSize().height);
+    }
+
+    private int displayY(double y) {
+        return (int) Math.round(y / pixelHeight());
+    }
+
+    private int displayX(double x) {
+        return (int) Math.round(x / pixelWidth());
+    }
+
+    private double pixelHeight() {
+        return rangeY / maxPixelHeight();
+    }
+
+    private double maxPixelHeight() {
+        return getSize().getWidth();
+    }
+
+    private double pixelWidth() {
+        return rangeX / maxPixelWidth();
+    }
+
+    private double maxPixelWidth() {
+        return getSize().getHeight();
+    }
+
+    private int pixelWidth(double rangeWidth) {
+        return (int) (rangeWidth / pixelWidth());
+    }
+
+    private int pixelHeight(double rangeHeight) {
+        return (int) (rangeHeight / pixelHeight());
     }
 }
---
+++ src/com/inothnagel/VisualBinarySearchTree/Node.java
@@ -0,0 +1,14 @@
+package com.inothnagel.VisualBinarySearchTree;
+
+/**
+ * Created by inothnagel on 2016/05/25.
+ */
+public class Node {
+    private final int value;
+    private double x;
+    private double y;
+
+    public Node(int i) {
+        this.value = i;
+    }
+}
